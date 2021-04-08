e6a20c44481e3f9ccd4f357bca0e68d5347d192f
Julián Sánchez <julian.sanchez@501labs.com.ar>
2021-04-08 16:35:20 -0300

    Se agrega telefono

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var vm = new Vue({
     data() {
         return {
             value: "2323",
+            phone: "",
         };
     },
     mounted() {
-        const selector = this.$refs.inputmask;
+        const selector = this.$refs.inputmask1;
+        const phone = this.$refs.inputmask2;
 
         Inputmask({ regex: "[0-9]*" }).mask(selector);
+        Inputmask({ regex: "(0)([0-9]{2,4}-)(15[0-9]{6,8})" }).mask(phone);
     },
 });
